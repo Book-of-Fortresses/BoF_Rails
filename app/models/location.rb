25f320bccf5e93eab0c92e2512c53acf772b7f67
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2018-03-16 12:27:44 -0400

    set up location routes/controller/model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,11 @@
 class Location < ApplicationRecord
+  has_many :perspective_drawings, :foreign_key => 'at_locations_table_id', :primary_key => 'at_locations_table_id'
+
+  has_many :images, :foreign_key => 'at_locations_table_id',
+  :primary_key => 'at_locations_table_id'
+
+  def to_param
+    slug
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :perspective_drawings
-  resources :images
-  resources :collaborators
-  resources :locations
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :locations, param: :slug, only: [:index, :show]
+
+  root to: "home#index"
+
 end
+
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
