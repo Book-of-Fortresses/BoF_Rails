56471fa2a54cbd5d7269de99f478e7971b27f93c
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2018-03-23 13:19:26 -0400

    basic styling

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,3 +15,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w( images/*)
 Rails.application.config.assets.precompile += %w( main.css )
+Rails.application.config.assets.precompile += %w( locations.css )
