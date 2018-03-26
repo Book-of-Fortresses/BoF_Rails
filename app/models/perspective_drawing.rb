b0741f0ea567ae8ddb99aab892d13242cb912fdd
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2018-03-26 13:14:12 -0400

    update model associations

--- app/models/perspective_drawing.rb
+++ app/models/perspective_drawing.rb
@@ -1,5 +1,3 @@
 class PerspectiveDrawing < ApplicationRecord
-  belongs_to :location, :foreign_key => 'at_locations_table_id', :primary_key => 'at_locations_table_id'
 
-  has_many :images, :foreign_key => 'at_perspective_drawings_table_id', :primary_key => 'at_perspective_drawings_table_id'
 end
