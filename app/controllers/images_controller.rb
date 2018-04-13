ac66450fedcfd73ecf8d69f0fa3cca0f9fcf9f40
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2018-04-13 14:59:00 -0400

    refactor airtable import

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,75 +1,24 @@
 class ImagesController < ApplicationController
   before_action :set_image, only: [:show, :edit, :update, :destroy]
 
-  # GET /images
-  # GET /images.json
   def index
     @images = Image.all
   end
 
-  # GET /images/1
-  # GET /images/1.json
   def show
     render :show
   end
 
-  # # GET /images/new
-  # def new
-  #   @image = Image.new
-  # end
-  #
-  # # GET /images/1/edit
-  # def edit
-  # end
-  #
-  # # POST /images
-  # # POST /images.json
-  # def create
-  #   @image = Image.new(image_params)
-  #
-  #   respond_to do |format|
-  #     if @image.save
-  #       format.html { redirect_to @image, notice: 'Image was successfully created.' }
-  #       format.json { render :show, status: :created, location: @image }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @image.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-  #
-  # # PATCH/PUT /images/1
-  # # PATCH/PUT /images/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @image.update(image_params)
-  #       format.html { redirect_to @image, notice: 'Image was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @image }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @image.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-  #
-  # # DELETE /images/1
-  # # DELETE /images/1.json
-  # def destroy
-  #   @image.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
-  # end
+  def create
+    @image = Image.new(image_params)
+  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_image
       @image = Image.find_by(slug: params[:slug])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
-      params.fetch(:image, {})
+      params.permit(:at_image_id, :slug, :at_images_table_id, :at_locations_table_id, :url, :filename, :size, :image_type, :image_category, :source, :page_number, :primary_site_name, :view_direction, :secondary_site_name, :secondary_site_kingdom, :number_of_watchtowers, :exterior_features, :status, :fortress_features, :agol_slide_embed, :at_images_table_time_created, :small_thumbnail_url, :small_thumbnail_width, :small_thumbnail_height, :large_thumbnail_url, :large_thumbnail_width, :large_thumbnail_height)
     end
 end
