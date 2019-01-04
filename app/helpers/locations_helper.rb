c5c292764fd57b2189a9c0758b01d3226ac54f0f
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2019-01-03 21:01:05 -0500

    edit secondary_sites method

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -3,8 +3,10 @@ module LocationsHelper
   def secondary_sites(location)
     ss = []
     location.images.each do |image|
-      unless image.secondary_site_name == 'None'
-        ss << image.secondary_site_name.split(',')
+      if image.secondary_site_name
+        unless image.secondary_site_name == 'None'
+          ss << image.secondary_site_name.split(',')
+        end
       end
     end
     ss = ss.flatten.map{|i| i.strip}.uniq.join(', ')
