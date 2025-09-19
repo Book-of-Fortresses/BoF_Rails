a12283680bb9f487fc79ae6118bc6c958d4011fb
JocelynTriplett <Jocelyn.Triplett@gmail.com>
2025-09-18 21:46:45 -0400

    update active storage

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,7 @@
+# config/boot.rb
+require 'logger'            # <= add this line first
+
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'bundler/setup'     # Set up gems listed in the Gemfile.
+# require 'bootsnap/setup'  # If you use bootsnap, leave it; it’s fine after 'logger'
